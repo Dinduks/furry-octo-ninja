d923b597a4288aca01aa9a7b1bc2160820fb2ee9
Dinduks <samy@dindane.com>
2012-05-06 03:31:00 +0200

    Change body column from String to Text type

--- models/snippet.rb
+++ models/snippet.rb
@@ -3,7 +3,7 @@ class Snippet
   property :id, Serial
   property :title, String
   property :slug, String
-  property :body, String
+  property :body, Text
   property :created_at, DateTime
   validates_presence_of   :title
   validates_uniqueness_of :slug
